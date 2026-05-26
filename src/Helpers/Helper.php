c53506d4e294305d438d612ead029d1ae485c735
priscillay-toast <92568971+priscillay-toast@users.noreply.github.com>
2026-05-26 15:42:40 +1200

    Add super admin check for administrator group

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -29,6 +29,10 @@ class Helper
     {
         $defaultUser = Environment::getEnv('SS_DEFAULT_ADMIN_USERNAME');
         $currentUser = Security::getCurrentUser();
+        // if $currentUSer is Administrator group, they are super admin regardless of username
+        if ($currentUser && $currentUser->inGroup('administrators')) {
+            return true;
+        }
         return $defaultUser && $currentUser && $currentUser->Email === $defaultUser;
     }
 
