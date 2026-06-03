0ee73be0eb94052f84ba2227c74eeb4fd859c7a4
priscillay-toast <92568971+priscillay-toast@users.noreply.github.com>
2026-06-03 12:31:43 +1200

    Add bypass check for super admin verification

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -29,9 +29,11 @@ class Helper
     {
         $defaultUser = Environment::getEnv('SS_DEFAULT_ADMIN_USERNAME');
         $currentUser = Security::getCurrentUser();
-        // if $currentUSer is Administrator group, they are super admin regardless of username
-        if ($currentUser && $currentUser->inGroup('administrators')) {
-            return true;
+        if(Config::inst()->get(self::class, 'bypass_super_admin_check')) {    
+            // if $currentUSer is Administrator group, they are super admin regardless of username
+            if ($currentUser && $currentUser->inGroup('administrators')) {
+                return true;
+            }
         }
         return $defaultUser && $currentUser && $currentUser->Email === $defaultUser;
     }
